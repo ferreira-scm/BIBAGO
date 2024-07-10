18e4c194897af5e8fb143523e8486aad4620ee24
ferreira-scm <susanaferreira@posteo.net>
2024-07-10 14:22:50 +0200

    Lat

--- R/3_FA.R
+++ R/3_FA.R
@@ -8,120 +8,124 @@ rownames(Perso) <- Perso$ID
 Perso$ID <- NULL
 
 
-##### all together
-# exclude sudden display dur, low KMO, doesn't cluster in the network
-Perso$NPT_sudden_display_dur <- NULL
-
 # scale and do the correlation matrix
 Perso.s<-scale(Perso)
 corr_P <- cor(Perso.s)
-#ggcorrplot(BisBas.s)
+#ggcorrplot(corr_P)
 
 cortest.mat(corr_P,n1=80)
 cortest.bartlett(corr_P,n=80)
 KMO(corr_P)
+## remove this one variable and calculate correlation matrix again
+Perso$NPT_sudden_display_dur <- NULL
+Perso.s<-scale(Perso)
+corr_P <- cor(Perso.s)
 
-fa.parallel(corr_P, n.obs=80, fm="pa", fa="fa")
-# 3-5
-
-library(nFactors)
-
-plot(nScree(x=corr_P, model="factors"))
-
-Perso_sol <- fa(corr_P, nfactors=5, rotate="oblimin", fm="pa", max.iter = 100)
-
-#Perso_dat <- fa(r=Perso.s, nfactors=5, rotate="oblimin", fm="pa", scores="regression")
-
-#pairs.panels(Perso_dat$scores)
-
-
-print(Perso_sol$loadings,cutoff = 0.3)
-
-fa.diagram(Perso_sol)
-
-Perso_sol$uniquenesses
-
-Perso_scores <- factor.scores(Perso.s, Perso_sol)
-
-Perso_scores <- as.data.frame(Perso_scores$scores)
-
-scores.dim <- data.frame(BAS=Perso_scores$PA1, BIS=Perso_scores$PA2, Asoc=Perso_scores$PA3, Shy=Perso_scores$PA4)
-
-head(scores.dim)
-
-
-Perso_sol
-
-################### only BIBAGO
-#fa.congruence(Perso_sol, Perso_dat)
-
-
+################### extended factor analysis
 l <- labels[which(labels$Label%in%names(Perso)),]
-# reordering
-l <- l[match(names(Perso), l$Label),]
-all(l$Label==names(Perso))# santiy check
 
-
-#### based on the network, we now reclassify voc_freq as BIS
-l$Hypothesis.dimension[l$Label=="OFT_voc_freq"] <- "Boldness"
-l$Hypothesis.dimension[l$Label=="BIBAGO_voc_freq"] <- "BIS"
-#l$Hypothesis.dimension[l$Label=="NPT_sudden_display_dur"] <- "BAS"
-
+l
 
 colnames(corr_P)
-
-set.seed(42)
-d <- sim.item(12)
-
 
 R <- cor(d)
 Ro <- corr_P[c(1,2,3,4,5,6,8,14, 17),c(1,2,3,4,5,6,8,14, 17)]
 Roe <- corr_P[c(1,2,3,4,5,6,8,14, 17),c(7, 9:13, 15,16, 18,19)]
 
+fa.parallel(Ro, n.obs=80, fm="pa", fa="fa")
+
 fo <- fa(Ro, nfactors=2, rotate="oblimin", fm="pa")
 fe <- fa.extension(Roe, fo)
 
-fe
-
-pdf("fig/FA.pdf", width=20, height=12)
+pdf("fig/FA.pdf", width=6, height=8)
 fa.diagram(fo, fe=fe)
 dev.off()
 
-fo
-
-#ba5 <- bassAckward(Ro, nfactors =c(2,3,4,5),plot=FALSE)
-#baf <- bassAckward.diagram(ba5)
-
 print(fo$loadings, cutoff = 0.3)
 
-BisBas <- Perso.s[,names(Perso)%in%l$Label[l$Hypothesis.dimension%in%c("BAS", "BIS")]]
+print(fe, digits=3)
 
-DimP <- Perso.s[,!names(Perso)%in%l$Label[l$Hypothesis.dimension%in%c("BAS", "BIS")]]
+### scores
+BisBas_scores <- factor.scores(Perso.s[,c(1,2,3,4,5,6,8,14, 17)], fo)
 
-# scale and do the correlation matrix
-BisBas.s<-scale(BisBas)
+BisBas_scores <- as.data.frame(BisBas_scores$scores)
 
-corr_BisBas <- cor(BisBas)
-#ggcorrplot(BisBas.s)
+head(BisBas_scores)
 
-cortest.mat(corr_BisBas,n1=80)
 
-cortest.bartlett(corr_BisBas,n=80)
-KMO(corr_BisBas)
+################### laterality testing
 
-fa.parallel(corr_BisBas, n.obs=80)
-# 2
+Perso.s <- as.data.frame(Perso.s)
+Perso.s$ID <- as.factor(rownames(Perso.s))
+Perso.s <- cbind(Perso.s, BisBas_scores)
+latS <- merge(latI, Perso.s, by="ID")
 
-BisBas_sol <- fa(corr_BisBas, nfactors=2, rotate="oblimin", fm="pa")
+#clustering based on laterality by Charlotte
+# scaling
+#latS<- latS %>% mutate_at(c("LI_w", "LI_f", "LI_t"), ~(scale(.) %>% as.vector))
 
-fe <- fa(corr_BisBas, nfactors=2, rotate="oblimin", fm="pa")
+km <- kmeans(latS[, c("LI_f", "LI_t")], centers=4, nstart=10)# we leave LI_w out because distribution is not binomial, therefore not a good indicator of laterality. We expect 4 clusters (2*2)
 
-BisBas_sol
+library(tidyverse)
 
-BisBas_scores <- factor.scores(BisBas.s, BisBas_sol)
+centroids <- as_tibble(km$centers, rownames="cluster")
 
-fa.extension(DimP, BisBas_sol,correct=TRUE)
+centroids# cluster 1 is RR and cluster 4 is LL
 
-print(BisBas_sol$loadings,cutoff = 0.3)
+latS$km <- as.factor(km$cluster)
 
-fa.diagram(BisBas_sol)
+latK <- latS[latS$km%in%c("1", "4"),]
+
+# just to make it pretty, I want a variable with 0 and 1
+latK$Lat[latK$km==1] <- 1
+latK$Lat[latK$km==4] <- 0
+
+latK$Lat2[latK$km==1] <- "Right"
+latK$Lat2[latK$km==4] <- "Left"
+
+
+ggplot(latK, aes(LI_f, LI_t, colour=km))+geom_point()
+
+#df <- df[, c("chew_dur", "freez_dur", "BIBAGO_inter_voc_dur", "expl_wob_lat", "BIBAGO_voc_freq")]
+
+##### compare only LL and RR #####
+
+names(latK)
+
+#BIBLat <- glm(Lat~chew_dur+ freez_dur+ expl_wob_dur+BIBAGO_inter_voc_dur + BIBAGO_voc_freq+choco_eaten, data = latK, family=binomial)
+#anova(BIBLat)
+#summary(BIBLat)
+
+BIBLatS <- glm(Lat~PA1+PA2, data = latK, family=binomial)
+
+anova(BIBLatS)
+summary(BIBLatS)
+
+Fig3A <- ggplot(BisBas_scores, aes(PA1, PA2))+
+    geom_point(size=4, alpha=0.6)+
+    theme_classic()
+
+Fig3B <- ggplot(latK, aes(Lat2, PA1, colour=Lat2))+
+    geom_boxplot(outlier.shape = NA)+
+    geom_jitter(width=0.2, height=NULL, size=4, alpha=0.6)+
+    labs(x="Laterality", y="PA1-BAS/FFFs")+
+    scale_colour_manual(values=c("#FFC107", "#004D40"))+
+    guides(color = "none", size = "none")+
+    theme_classic()
+
+
+Fig3C <- ggplot(latK, aes(Lat2, PA2, colour=Lat2))+
+    geom_boxplot(outlier.shape = NA)+
+    geom_jitter(width=0.2, height=NULL, size=4, alpha=0.6)+
+    labs(x="Laterality", y="PA2-BIS/FFFs")+
+    scale_colour_manual(values=c("#FFC107", "#004D40"))+
+    guides(color = "none", size = "none")+
+    theme_classic()
+
+library(cowplot)
+
+BC <- cowplot::plot_grid(Fig3B, Fig3C, labels=c("B", "C"))
+
+Fig3 <- cowplot::plot_grid(Fig3A, BC, ncol=1, labels=c("A", ""), rel_heights=c(2,1))
+
+ggsave("fig/Figure3.pdf", Fig3, width = 180, height = 180, dpi = 300, units="mm")
